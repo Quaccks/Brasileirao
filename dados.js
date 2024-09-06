f3502f78a7a8f34255d80e22b41ea2ef637cd60b
Limeira <rafinha.limeira2016@gmail.com>
2024-09-06 13:19:22 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -1,26 +1,154 @@
 let dados = [
-    {
-      titulo: "Palmeiras",
-      descricao: "O Palmeiras é um dos clubes de futebol mais tradicionais e populares do Brasil. Fundado em 26 de agosto de 1914, o Verdão possui uma rica história marcada por grandes conquistas tanto no cenário nacional quanto internacional.",
-      link: "https://pt.wikipedia.org/wiki/Sociedade_Esportiva_Palmeiras",
-      instagram: "https://www.instagram.com/palmeiras/",
-      tags: "palmeiras verdão verdao alviverde",
-      regiao: "sp paulista"
-    },
-    {
-      titulo: "Grêmio",
-      descricao: "O Grêmio Foot-Ball Porto Alegrense é um dos clubes de futebol mais tradicionais do Brasil, com uma história rica em títulos e rivalidades. Fundado em 18 de setembro de 1903, o Tricolor Imortal é conhecido por sua paixão e tradição.",
-      link: "https://pt.wikipedia.org/wiki/Grêmio_Foot-Ball_Porto_Alegrense",
-      instagram:"https://www.instagram.com/gremio/",
-      tags: "tricolor imortal rei de copas",
-      regiao: "rs gaucho"
-    },
-    {
-      titulo: "Vasco da Gama",
-      descricao: "O Vasco da Gama é um dos clubes de futebol mais populares do Brasil, com uma grande torcida e uma história vitoriosa. Fundado em 21 de agosto de 1898, o Gigante da Colina é conhecido por sua paixão e tradição.",
-      link: "https://pt.wikipedia.org/wiki/Vasco_da_Gama",
-      instagram: "https://www.instagram.com/vascodagama/",
-      tags: "gigante da colina vascão vascao ",
-      regiao: "rj carioca"
-    }
-  ];
+  {
+    titulo: "Atlético Goianiense",
+    descricao: "O Atlético Clube Goianiense é um clube tradicional do futebol brasileiro, fundado em 2 de abril de 1937. Conhecido como Dragão, o clube tem uma história marcada por grandes conquistas regionais.",
+    link: "https://pt.wikipedia.org/wiki/Atl%C3%A9tico_Clube_Goianiense",
+    instagram: "https://www.instagram.com/atleticogoianiense/",
+    tags: "dragão goiano, atleticogo",
+    regiao: "go goiano"
+  },
+  {
+    titulo: "Atlético Mineiro",
+    descricao: "O Clube Atlético Mineiro, também conhecido como Galo, é um dos maiores clubes do futebol brasileiro. Fundado em 25 de março de 1908, o clube tem uma rica história de títulos e grandes torcidas.",
+    link: "https://pt.wikipedia.org/wiki/Clube_Atl%C3%A9tico_Mineiro",
+    instagram: "https://www.instagram.com/atletico/",
+    tags: "galo, galo forte vingador, atleticomg",
+    regiao: "mg mineiro"
+  },
+  {
+    titulo: "Atlético Paranaense",
+    descricao: "O Clube Atlético Paranaense, atualmente conhecido como Athletico Paranaense, é um dos clubes mais tradicionais do futebol brasileiro, com uma rica história e grande torcida. Fundado em 26 de março de 1924, o clube é famoso por suas conquistas nacionais e internacionais.",
+    link: "https://pt.wikipedia.org/wiki/Club_Athletico_Paranaense",
+    instagram: "https://www.instagram.com/athleticopr/",
+    tags: "furacão, campeão paranaense, athletico",
+    regiao: "pr paranaense"
+  },
+  {
+    titulo: "Bahia",
+    descricao: "O Esporte Clube Bahia é um dos clubes mais tradicionais do futebol brasileiro, fundado em 1 de janeiro de 1931. O Tricolor de Aço é conhecido por sua grande torcida e por suas conquistas no cenário nacional.",
+    link: "https://pt.wikipedia.org/wiki/Esporte_Clube_Bahia",
+    instagram: "https://www.instagram.com/ecbahia/",
+    tags: "tricolor de aço, bahia",
+    regiao: "ba baiano"
+  },
+  {
+    titulo: "Botafogo",
+    descricao: "O Botafogo de Futebol e Regatas é um dos clubes mais tradicionais do Brasil, com uma rica história no futebol. Fundado em 12 de agosto de 1904, o Glorioso é famoso por revelar grandes craques do futebol brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Botafogo_de_Futebol_e_Regatas",
+    instagram: "https://www.instagram.com/botafogo/",
+    tags: "glorioso, fogão, botafogo",
+    regiao: "rj carioca"
+  },
+  {
+    titulo: "Bragantino",
+    descricao: "O Red Bull Bragantino, fundado em 8 de janeiro de 1928, é um clube em ascensão no futebol brasileiro, representando a cidade de Bragança Paulista com grandes conquistas recentes.",
+    link: "https://pt.wikipedia.org/wiki/Red_Bull_Bragantino",
+    instagram: "https://www.instagram.com/redbullbragantino/",
+    tags: "massa bruta, bragantino",
+    regiao: "sp paulista"
+  },
+  {
+    titulo: "Criciúma",
+    descricao: "O Criciúma Esporte Clube, fundado em 13 de maio de 1947, é um dos principais clubes de Santa Catarina. O Tigre é conhecido por suas conquistas regionais e sua torcida apaixonada.",
+    link: "https://pt.wikipedia.org/wiki/Crici%C3%BAma_Esporte_Clube",
+    instagram: "https://www.instagram.com/criciumaoficial/",
+    tags: "tigre, criciuma",
+    regiao: "sc catarinense"
+  },
+  {
+    titulo: "Cruzeiro",
+    descricao: "O Cruzeiro Esporte Clube, fundado em 2 de janeiro de 1921, é um dos clubes mais tradicionais do futebol brasileiro, com uma rica história de títulos nacionais e internacionais.",
+    link: "https://pt.wikipedia.org/wiki/Cruzeiro_Esporte_Clube",
+    instagram: "https://www.instagram.com/cruzeiro/",
+    tags: "raposa, cruzeiro",
+    regiao: "mg mineiro"
+  },
+  {
+    titulo: "Cuiabá",
+    descricao: "O Cuiabá Esporte Clube, fundado em 12 de dezembro de 2001, é um clube em ascensão no futebol brasileiro, representando o estado de Mato Grosso com grande crescimento e conquistas recentes.",
+    link: "https://pt.wikipedia.org/wiki/Cuiab%C3%A1_Esporte_Clube",
+    instagram: "https://www.instagram.com/cuiabaec/",
+    tags: "dourado, cuiabá",
+    regiao: "mt mato-grossense"
+  },
+  {
+    titulo: "Flamengo",
+    descricao: "O Clube de Regatas do Flamengo, fundado em 17 de novembro de 1895, é um dos clubes mais populares do Brasil. O Rubro-Negro tem uma das maiores torcidas do mundo e uma história rica em títulos nacionais e internacionais.",
+    link: "https://pt.wikipedia.org/wiki/Clube_de_Regatas_do_Flamengo",
+    instagram: "https://www.instagram.com/flamengo/",
+    tags: "rubro-negro, mengão, flamengo",
+    regiao: "rj carioca"
+  },
+  {
+    titulo: "Fluminense",
+    descricao: "O Fluminense Football Club, fundado em 21 de julho de 1902, é um dos clubes mais tradicionais do futebol brasileiro, conhecido como o Tricolor das Laranjeiras e famoso por sua rica história.",
+    link: "https://pt.wikipedia.org/wiki/Fluminense_Football_Club",
+    instagram: "https://www.instagram.com/fluminensefc/",
+    tags: "tricolor, flu, fluminense",
+    regiao: "rj carioca"
+  },
+  {
+    titulo: "Fortaleza",
+    descricao: "O Fortaleza Esporte Clube, fundado em 18 de outubro de 1918, é um dos clubes mais tradicionais do futebol cearense, conhecido como Leão do Pici.",
+    link: "https://pt.wikipedia.org/wiki/Fortaleza_Esporte_Clube",
+    instagram: "https://www.instagram.com/fortalezaec/",
+    tags: "leão do pici, fortaleza",
+    regiao: "ce cearense"
+  },
+  {
+    titulo: "Grêmio",
+    descricao: "O Grêmio Foot-Ball Porto Alegrense é um dos clubes de futebol mais tradicionais do Brasil, com uma história rica em títulos e rivalidades. Fundado em 18 de setembro de 1903, o Tricolor Imortal é conhecido por sua paixão e tradição.",
+    link: "https://pt.wikipedia.org/wiki/Grêmio_Foot-Ball_Porto_Alegrense",
+    instagram: "https://www.instagram.com/gremio/",
+    tags: "tricolor imortal rei de copas",
+    regiao: "rs gaúcho"
+  },
+  {
+    titulo: "Internacional",
+    descricao: "O Sport Club Internacional, fundado em 4 de abril de 1909, é um dos maiores clubes do futebol brasileiro, conhecido como Colorado, com uma rica história de conquistas.",
+    link: "https://pt.wikipedia.org/wiki/Sport_Club_Internacional",
+    instagram: "https://www.instagram.com/scinternacional/",
+    tags: "colorado, inter, internacional",
+    regiao: "rs gaúcho"
+  },
+  {
+    titulo: "Juventude",
+    descricao: "O Esporte Clube Juventude, fundado em 29 de junho de 1913, é um dos clubes mais tradicionais do Rio Grande do Sul, com uma história marcada por conquistas estaduais.",
+    link: "https://pt.wikipedia.org/wiki/Esporte_Clube_Juventude",
+    instagram: "https://www.instagram.com/ecjuventudeoficial/",
+    tags: "juventude, alviverde",
+    regiao: "rs gaúcho"
+  },
+  {
+    titulo: "Palmeiras",
+    descricao: "O Palmeiras é um dos clubes de futebol mais tradicionais e populares do Brasil. Fundado em 26 de agosto de 1914, o Verdão possui uma rica história marcada por grandes conquistas tanto no cenário nacional quanto internacional.",
+    link: "https://pt.wikipedia.org/wiki/Sociedade_Esportiva_Palmeiras",
+    instagram: "https://www.instagram.com/palmeiras/",
+    tags: "palmeiras verdão verdão alviverde",
+    regiao: "sp paulista"
+  },
+  {
+    titulo: "São Paulo",
+    descricao: "O São Paulo Futebol Clube, fundado em 25 de janeiro de 1930, é um dos clubes mais tradicionais e vitoriosos do futebol brasileiro, conhecido como Tricolor Paulista.",
+    link: "https://pt.wikipedia.org/wiki/S%C3%A3o_Paulo_Futebol_Clube",
+    instagram: "https://www.instagram.com/saopaulofc/",
+    tags: "tricolor paulista, são paulo",
+    regiao: "sp paulista"
+  },
+  {
+    titulo: "Vasco da Gama",
+    descricao: "O Vasco da Gama é um dos clubes de futebol mais populares do Brasil, com uma grande torcida e uma história vitoriosa. Fundado em 21 de agosto de 1898, o Gigante da Colina é conhecido por sua paixão e tradição.",
+    link: "https://pt.wikipedia.org/wiki/Vasco_da_Gama",
+    instagram: "https://www.instagram.com/vascodagama/",
+    tags: "gigante da colina vascão vasco",
+    regiao: "rj carioca"
+  },
+  {
+    titulo: "Vitória",
+    descricao: "O Esporte Clube Vitória, fundado em 13 de maio de 1899, é um dos clubes mais tradicionais da Bahia e do futebol brasileiro, conhecido como o Leão da Barra.",
+    link: "https://pt.wikipedia.org/wiki/Esporte_Clube_Vit%C3%B3ria",
+    instagram: "https://www.instagram.com/ecvitoria/",
+    tags: "leão da barra, vitória vitoria",
+    regiao: "ba baiano"
+  }
+];
